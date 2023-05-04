6d523a797f9874f5ebf4228d4e8a33f33ef2162f
Lykichu <132097712+Lykichu@users.noreply.github.com>
2023-05-04 16:45:13 -0600

    Update pokedex.js

--- pokedex.js
+++ pokedex.js
@@ -3,6 +3,8 @@ var pokedex = {}; // {1 : {"term" : "linear equation", "name" : "bulbsaur", "img
 
 window.onload = async function() {
     // getPokemon(1);
+
+
     for (let i = 1; i <= pokemonCount; i++) {
         await getPokemon(i);
         //<div id="1" class="pokemon-name">BULBASAUR</div>
